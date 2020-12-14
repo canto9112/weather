63e25b95f346ea47ac948177d362f0c38203bf4b
canto9112 <cantoweb112@gmail.com>
2020-12-14 19:43:15 +0300

    правки

--- weather.py
+++ weather.py
@@ -3,10 +3,14 @@ import requests
 
 citys = ['Лондон','Череповец','Шереметьево']
 
-url_template = 'http://wttr.in/{}?nTqM&lang=ru'
+payload = {"nTqM&lang":"ru"}
+
+url_template = 'http://wttr.in/{}'
 for town in citys:
     url = url_template.format(town)
 
-    response = requests.get(url)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
     print(response.text)
+
+
