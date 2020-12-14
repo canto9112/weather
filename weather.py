da8063879500403a3d9afd2502d20191d58ba418
canto9112 <cantoweb112@gmail.com>
2020-12-14 20:09:24 +0300

    исправил опечатку2

--- weather.py
+++ weather.py
@@ -1,12 +1,12 @@
 import requests
 
 
-citys = ['Лондон','Череповец','Шереметьево']
+cities = ['Лондон','Череповец','Шереметьево']
 
 payload = {"lang":"ru","n":"","T":"","q":"","m":""}
 
 url_template = 'http://wttr.in/{}'
-for city in citys:
+for city in cities:
     url = url_template.format(city)
 
     response = requests.get(url, params=payload)
